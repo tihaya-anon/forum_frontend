c58bdba55fb816f92f1d4d7b0bc74a31c4ba6633
yxluo <yuxiangluo2003@gmail.com>
2025-01-14 21:27:15 +0800

    feat/postcard, remain color

--- src/components/forum/post/PostCard.tsx
+++ src/components/forum/post/PostCard.tsx
@@ -1,7 +1,13 @@
 import type { FC, ReactNode } from "react";
 import React, { memo } from "react";
 import { PostPersist } from "@/types/vo/post";
-import { FaComment, FaThumbsUp, FaThumbsDown, FaCalendarAlt, FaUser } from "react-icons/fa";  // 导入所需的图标
+import {
+  FaComment,
+  FaThumbsUp,
+  FaThumbsDown,
+  FaCalendarAlt,
+  FaUser,
+} from "react-icons/fa"; // 导入所需的图标
 
 interface IProps {
   children?: ReactNode;
@@ -9,10 +15,26 @@ interface IProps {
 }
 
 const PostCard: FC<IProps> = ({ post }) => {
+  const currentDate = new Date();
+  const createDate = new Date(post.createAt);
+
+  const isToday =
+    currentDate.getDate() === createDate.getDate() &&
+    currentDate.getMonth() === createDate.getMonth() &&
+    currentDate.getFullYear() === createDate.getFullYear();
+
+  const formattedDate = isToday
+    ? createDate.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      })
+    : createDate.toLocaleDateString();
   return (
     <div className="bg-white p-5 rounded-lg shadow-lg max-w-xl mx-auto my-4">
       {/* Title */}
-      <div className="text-2xl font-bold mb-4">{post.title}</div>
+      <div className="text-2xl font-bold mb-4">
+        {post.title}
+      </div>
 
       {/* Tag List */}
       <div className="flex flex-wrap gap-2 mb-4">
@@ -27,13 +49,14 @@ const PostCard: FC<IProps> = ({ post }) => {
       </div>
 
       {/* Created At and Username */}
-      <div className="flex text-sm text-gray-600 mb-4">
-        {/* Created At Icon */}
-        <div className="flex items-center space-x-1 mr-4">
+      <div className="flex justify-between text-sm text-gray-600 mb-4">
+        {/* Created At Icon (on the left) */}
+        <div className="flex items-center space-x-1">
           <FaCalendarAlt className="text-lg" />
-          <span>{post.createAt.toLocaleDateString()}</span>
+          <span>{formattedDate}</span>
         </div>
-        {/* Author Icon */}
+
+        {/* Author Icon (on the right) */}
         <div className="flex items-center space-x-1">
           <FaUser className="text-lg" />
           <span>{post.username}</span>
